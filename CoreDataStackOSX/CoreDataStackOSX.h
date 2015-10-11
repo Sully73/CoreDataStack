559e58720c2aee253e81d46fa3d4a9d39bb894f8
Cory Sullivan <cory@solbits.com>
2015-10-11 18:15:15 -0300

    Add OSX target

---
+++ CoreDataStackOSX/CoreDataStackOSX.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataStackOSX.h
+//  CoreDataStackOSX
+//
+//  Created by Cory Sullivan on 10/11/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CoreDataStackOSX.
+FOUNDATION_EXPORT double CoreDataStackOSXVersionNumber;
+
+//! Project version string for CoreDataStackOSX.
+FOUNDATION_EXPORT const unsigned char CoreDataStackOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataStackOSX/PublicHeader.h>
+
+
